7ea7b49eee1abd6cd7b40fa8c5ba234294c2c199
xhos <60789741+xhos@users.noreply.github.com>
2025-01-16 23:41:15 -0500

    first commit

---
+++ flake.nix
@@ -0,0 +1,55 @@
+{
+  description = "desktop shell that might be usable at some point, no promises";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    ags = {
+      url = "github:aylur/ags";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ags,
+  }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
+      default = ags.lib.bundle {
+        inherit pkgs;
+        src = ./.;
+        name = "my-shell";
+        entry = "app.ts";
+
+        # additional libraries and executables to add to gjs' runtime
+        extraPackages = [
+          ags.packages.${pkgs.system}.battery
+          ags.packages.${pkgs.system}.bluetooth
+          ags.packages.${pkgs.system}.hyprland
+          ags.packages.${pkgs.system}.network
+          ags.packages.${pkgs.system}.mpris
+          ags.packages.${pkgs.system}.tray
+          ags.packages.${pkgs.system}.wireplumber
+        ];
+      };
+    };
+
+    devShells.${system} = {
+      default = pkgs.mkShell {
+        buildInputs = [
+          # TODO: figure out how to use this for cleaner dev experience
+          # includes astal3 astal4 astal-io by default
+          (ags.packages.${system}.default.override {
+            extraPackages = [
+              # cherry pick packages
+            ];
+          })
+        ];
+      };
+    };
+  };
+}
