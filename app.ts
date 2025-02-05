3640b38412f6b7cd7a70de785d719ee9f456f419
xhos <60789741+xhos@users.noreply.github.com>
2025-02-05 17:02:05 -0500

    chore: scss hot reloading

--- app.ts
+++ app.ts
@@ -1,10 +1,31 @@
 import { App } from 'astal/gtk4';
 import style from './style.scss';
+import { exec, monitorFile } from 'astal';
 import Bar from './widget/Bar';
 
+const scss = './style.scss'; // Path to your style file
+const css = '/tmp/style.css'; // Could be anywhere else
+
+compileScss(); // Precompile SCSS on startup
+
+// Monitor your SCSS file for changes and reload the CSS in the app
+monitorFile(scss, () => {
+  console.log('CSS RELOADED'); // Debug: log reloading action
+  compileScss();
+  App.reset_css();
+  App.apply_css(css);
+});
+
 App.start({
-  css: style,
+  css: css,
   main() {
     App.get_monitors().map(Bar);
   },
 });
+
+function compileScss() {
+  const error = exec(`sass ${scss} ${css}`);
+  if (error) {
+    console.error(`There went something wrong compiling the SCSS: ${error}`);
+  }
+}
