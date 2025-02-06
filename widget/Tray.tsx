c2241b7d8361a96aafaf7b9a88c3a811a4647cc1
xhos <60789741+xhos@users.noreply.github.com>
2025-02-06 00:50:41 -0500

    ✨: update systray widget to display popovers

--- widget/Tray.tsx
+++ widget/Tray.tsx
@@ -1,16 +1,36 @@
 import { Gtk } from 'astal/gtk4';
 import { Variable, bind } from 'astal';
+import { astalify, hook } from 'astal/gtk4';
 import { ArrowUpIcon } from './icons/arrow-up';
 import Tray from 'gi://AstalTray';
 
+const DEBUG = false; // Toggle debug tray name logging
+const excludeIcons = [/^video-display/, /^blueman-active/];
+
+const PopoverMenu = astalify<Gtk.PopoverMenu, Gtk.PopoverMenu.ConstructorProps>(
+  Gtk.PopoverMenu,
+  {}
+);
+
 function TrayItem(item: Tray.TrayItem) {
-  return (
-    <image
+  const button = (
+    <menubutton
       cssClasses={['TrayIcon']}
-      gicon={bind(item, 'gicon')}
       tooltipMarkup={bind(item, 'tooltipMarkup')}
-    />
-  );
+      setup={(self: Gtk.MenuButton) => {
+        self.insert_action_group('dbusmenu', item.actionGroup);
+      }}
+    >
+      <image gicon={bind(item, 'gicon')} />
+      <PopoverMenu menuModel={bind(item, 'menuModel')} flags={Gtk.PopoverMenuFlags.SLIDING} />
+    </menubutton>
+  ) as Gtk.MenuButton; // Cast button to MenuButton type
+
+  hook(button, item, 'notify::action-group', (self: Gtk.MenuButton) => {
+    self.insert_action_group('dbusmenu', item.actionGroup);
+  });
+
+  return button;
 }
 
 export default function Systray() {
@@ -22,7 +42,25 @@ export default function Systray() {
         <image cssClasses={['TrayIcon']} paintable={ArrowUpIcon()} />
       </box>
       <box cssClasses={['TrayExpanded']} orientation={Gtk.Orientation.VERTICAL}>
-        {bind(tray, 'items').as((items) => items.map(TrayItem))}
+        {bind(tray, 'items').as((items) => {
+          if (DEBUG) {
+            items.forEach((item) => {
+              const iconName = item.gicon?.to_string() || '';
+              console.log('Tray item:', {
+                name: item,
+                icon: iconName,
+                tooltip: item.tooltipMarkup,
+              });
+            });
+          }
+
+          return items
+            .filter((item) => {
+              const iconName = item.gicon?.to_string() || '';
+              return !excludeIcons.some((regex) => regex.test(iconName));
+            })
+            .map(TrayItem);
+        })}
       </box>
     </box>
   );
