9bf6b3e42d64ac96b5e7fae9d9bfb3be12c55ed6
xhos <60789741+xhos@users.noreply.github.com>
2025-02-06 23:50:56 -0500

    yup hot reloading is what caused errors

--- app.ts
+++ app.ts
@@ -1,20 +1,6 @@
 import { App } from 'astal/gtk4';
-import { exec, monitorFile } from 'astal';
 import Bar from './widget/Bar';
 import style from './style.scss';
-
-// const scss = './style.scss'; // Path to your style file
-// const css = '/tmp/style.css'; // Could be anywhere else
-
-// compileScss(); // Precompile SCSS on startup
-
-// Monitor your SCSS file for changes and reload the CSS in the app
-// monitorFile(scss, () => {
-//   console.log('CSS RELOADED'); // Debug: log reloading action
-//   compileScss();
-//   App.reset_css();
-//   App.apply_css(css);
-// });
 
 App.start({
   css: style,
@@ -22,10 +8,3 @@ App.start({
     App.get_monitors().map(Bar);
   },
 });
-
-// function compileScss() {
-//   const error = exec(`sass ${scss} ${css}`);
-//   if (error) {
-//     console.error(`There went something wrong compiling the SCSS: ${error}`);
-//   }
-// }
