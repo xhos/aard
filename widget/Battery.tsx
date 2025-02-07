6bff6d3fbba5f1571576c8ea71ef8ee1478389cc
xhos <60789741+xhos@users.noreply.github.com>
2025-02-07 11:16:12 -0500

    battery was not polled

--- widget/Battery.tsx
+++ widget/Battery.tsx
@@ -1,17 +1,24 @@
 import { Gtk } from 'astal/gtk4';
 import Battery from 'gi://AstalBattery';
+import { Variable, bind } from 'astal';
+
+// Poll battery level every second
+const batteryLevel = Variable(0).poll(1000, () => {
+  const battery = Battery.get_default();
+  return battery ? Math.round(battery.percentage * 100) : 0;
+});
 
 export default function BatteryWidget() {
-  const battery = Battery.get_default();
-  const percentage = battery ? Math.round(battery.percentage * 100) : 0;
-
   return (
     <box
       cssClasses={['BatteryBox']}
       halign={Gtk.Align.CENTER}
       orientation={Gtk.Orientation.VERTICAL}
     >
-      <label cssClasses={['BatteryLabel']} label={`${percentage}`} />
+      <label
+        cssClasses={['BatteryLabel']}
+        label={bind(batteryLevel).as((x: number) => x.toString())}
+      />
     </box>
   );
 }
