b512c461b58d81115540965cd589a2a5d4cd0133
xhos <60789741+xhos@users.noreply.github.com>
2025-02-06 23:48:20 -0500

    removed hot css reloading

--- app.ts
+++ app.ts
@@ -1,30 +1,31 @@
 import { App } from 'astal/gtk4';
 import { exec, monitorFile } from 'astal';
 import Bar from './widget/Bar';
+import style from './style.scss';
 
-const scss = './style.scss'; // Path to your style file
-const css = '/tmp/style.css'; // Could be anywhere else
+// const scss = './style.scss'; // Path to your style file
+// const css = '/tmp/style.css'; // Could be anywhere else
 
-compileScss(); // Precompile SCSS on startup
+// compileScss(); // Precompile SCSS on startup
 
 // Monitor your SCSS file for changes and reload the CSS in the app
-monitorFile(scss, () => {
-  console.log('CSS RELOADED'); // Debug: log reloading action
-  compileScss();
-  App.reset_css();
-  App.apply_css(css);
-});
+// monitorFile(scss, () => {
+//   console.log('CSS RELOADED'); // Debug: log reloading action
+//   compileScss();
+//   App.reset_css();
+//   App.apply_css(css);
+// });
 
 App.start({
-  css: css,
+  css: style,
   main() {
     App.get_monitors().map(Bar);
   },
 });
 
-function compileScss() {
-  const error = exec(`sass ${scss} ${css}`);
-  if (error) {
-    console.error(`There went something wrong compiling the SCSS: ${error}`);
-  }
-}
+// function compileScss() {
+//   const error = exec(`sass ${scss} ${css}`);
+//   if (error) {
+//     console.error(`There went something wrong compiling the SCSS: ${error}`);
+//   }
+// }
